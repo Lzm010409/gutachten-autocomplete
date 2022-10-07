85dcdb401cbe11d9b95997991a65bb0c62eae20d
Luke Gollenstede <lukegollenstede@gmail.com>
2022-10-07 15:00:32 +0200

    In AufnahmebogenServiceImpl, die Methode writeFahrzeugData um die Möglichkeit zur Eintragung von behobenen und nicht behoben Vorschäden erweitert. ReadCoordinates angepasst, da sich fehler Eingeschlichen hatten.

--- gutachten_autocomplet/src/main/java/lukegoll/gutachten_autocomplete/main/Main.java
+++ gutachten_autocomplet/src/main/java/lukegoll/gutachten_autocomplete/main/Main.java
@@ -1,12 +1,9 @@
 package lukegoll.gutachten_autocomplete.main;
 
-import java.io.IOException;
-
 import lukegoll.gutachten_autocomplete.fall.Fahrzeug;
+import lukegoll.gutachten_autocomplete.fall.Fall;
 import lukegoll.gutachten_autocomplete.fall.Kunde;
 import lukegoll.gutachten_autocomplete.itextpdf.AufnahmebogenServiceImpl;
-import lukegoll.gutachten_autocomplete.itextpdf.ReadCoordinates;
-import net.sourceforge.tess4j.TesseractException;
 
 public class Main {
 
@@ -14,19 +11,13 @@ public class Main {
 		AufnahmebogenServiceImpl a1 = new AufnahmebogenServiceImpl();
 		Kunde k1 = new Kunde();
 		Fahrzeug f1 = new Fahrzeug();
-		/*a1.writeKundenData(k1);
+		Fall fall1 = new Fall();
+		a1.writeKundenData(k1);
 		a1.writeKundenMail(k1);
 		a1.writeKundenTel(k1);
 		a1.writeFahrzeugData(f1);
-		
-		
-		System.out.println(f1.getKennzeichen());
-		f1.universalSetter("abc", 0);
-		System.out.println(f1.universalGetter(0));
-		System.out.println(f1.getKennzeichen());*/
-		
-		System.out.println(a1.readFileWithReturn(ReadCoordinates.FALLGZUSTAND));
-		
+		a1.writeFallData(fall1);
+		System.out.println(fall1.getSchadenDatumOrt());
 	}
 
 }
